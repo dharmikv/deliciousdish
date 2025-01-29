2cee95ad548fb4e5eeaa9fd6d27dc21cd609b895
crea8ivedev <dharmikv.encircle@gmail.com>
2025-01-29 17:56:51 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".culture-slider", {
+var culture_swiper = new Swiper(".culture-slider", {
     slidesPerView: 1,
     loop:true, // Default for small screens
     breakpoints: {
@@ -14,13 +14,33 @@ var swiper = new Swiper(".culture-slider", {
     }
 });
 
+var tesimonial_swiper = new Swiper(".tesmonial-swiper", {
+    slidesPerView: 1,
+    loop:true, // Default for small screens
+    breakpoints: {
+        640: {
+            slidesPerView: 1, // For screens >= 640px
+        },
+        768: {
+            slidesPerView: 2, // For screens >= 768px
+        },
+        1440: {
+            slidesPerView: 3, // For screens >= 1024px (desktop)
+        }
+    }
+});
+
+
+
+
+
 jQuery(document).ready(function ($) { 
-    if ($(window).scrollTop() >= 70) {
+    if ($(window).scrollTop() >= 40) {
         $('.header').addClass('is-sticky');
         $('.top-bar').addClass('hidden');
     }
     $(window).scroll(function () { 
-        if ($(window).scrollTop() >= 70) {
+        if ($(window).scrollTop() >= 40) {
             $('.header').addClass('is-sticky');
             $('.top-bar').addClass('hidden');
         } else {
@@ -28,4 +48,54 @@ jQuery(document).ready(function ($) {
             $('.top-bar').removeClass('hidden');
         }
     });
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sectionanim = gsap.utils.toArray('.fade-in');
+sectionanim.forEach((box) => {
+  gsap.fromTo(box, 
+    { autoAlpha: 0,y:30 }, 
+    { 
+      duration: 1, 
+      y:0,
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: box,
+        start: 'top 80%',  
+        end: 'top 50%',    
+        toggleActions: 'play none none none',
+        once: true
+      }
+    }
+  );
+});
+
+let open_btn = document.querySelector(".open-button");
+let sidebar = document.querySelector(".sidebar");
+let close_btn = document.querySelector(".close-btn");
+let sidebar_links = document.querySelectorAll(".sidebar ul li ");
+let sidebar_timeline = gsap.timeline();
+
+sidebar_timeline.to(sidebar, {
+  right: 0,
+  duration: 0.3,
+});
+sidebar_timeline.from(sidebar_links, {
+  x: 150,
+  opacity: 0,
+  duration: 0.3,
+  stagger: 0.1,
+});
+sidebar_timeline.pause();
+
+open_btn.addEventListener("click", function () {
+  sidebar_timeline.play();
+  document.body.style.overflow = "hidden";
+});
+
+close_btn.addEventListener("click", function () {
+  sidebar_timeline.reverse();
+  document.body.style.overflow = "";
 });
